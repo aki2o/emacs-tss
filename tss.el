35c60c40e58e5189892779a7d6f6e690bcefc054
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2014-08-28 20:25:37 +0900

    fix #8

--- tss.el
+++ tss.el
@@ -280,7 +280,9 @@
         (tss--debug "Start run flymake")
         (tss--sync-server)
         (setq flymake-last-change-time nil)
-        (setq flymake-check-start-time (flymake-float-time))
+        (setq flymake-check-start-time (if (functionp 'flymake-float-time)
+                                           (flymake-float-time)
+                                         (float-time)))
         (let* ((ret (tss--get-server-response "showErrors"
                                               :waitsec 2
                                               :response-start-char "["
